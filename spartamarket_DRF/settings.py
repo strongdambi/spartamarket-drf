ad58e547553c9de1f598acba907cceb7dfb30236
strongdambi <thekeytohong@gmail.com>
2024-09-04 10:24:45 +0900

    feat: signup, signin, token refresh postman 확인
    - signup, signin, token refresh
     - postman으로 테스트 완료
     - username, password 검증
     - token black list 확인

--- spartamarket_DRF/settings.py
+++ spartamarket_DRF/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django_seed',
     'rest_framework',
     'rest_framework_simplejwt',
+    'rest_framework_simplejwt.token_blacklist',
     #Local
     'accounts',
     'products',
@@ -102,10 +103,11 @@ REST_FRAMEWORK = {
 
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
+    "UPDATE_LAST_LOGIN": False,
 }
 
 
